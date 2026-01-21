47a79d25d88771be652714d6b26562a6972e44e8
Josh Zeng <142065053+zengjosh@users.noreply.github.com>
2026-01-21 17:05:23 -0500

    Delete refresh directory

--- refresh/src/alm_sdk/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2026-present zengjosh <zeng080407@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- refresh/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2026-present zengjosh <zeng080407@gmail.com>
-#
-# SPDX-License-Identifier: MIT
